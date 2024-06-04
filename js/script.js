017e8ad47b14807e2a8f1d841d886ed1d3e5fa46
luuxuanbach <87297106+luuxuanbach@users.noreply.github.com>
2024-06-04 15:57:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -69,16 +69,16 @@ myDate.innerHTML = yes;
 // Set onclick event for project cards
 var cardNodes = document.querySelector('.project').querySelectorAll('div.card');
 for (var i = 0; i < cardNodes.length; i++) {
-  var link = cardNodes[i].querySelector('a').getAttribute('href');
-  if (link) {
-    cardNodes[i].addEventListener('click', (event) => {
-      // Check if the clicked target is the <a> tag
-      if (event.target.tagName.toLowerCase() === 'a') {
-        // Stop the event from bubbling up to the parent div
-        event.stopPropagation();
-      } else {
+  cardNodes[i].addEventListener('click', (event) => {
+    // Check if the clicked target is the <a> tag
+    if (event.target.tagName.toLowerCase() === 'a') {
+      // Stop the event from bubbling up to the parent div
+      event.stopPropagation();
+    } else {
+      var link = cardNodes[i].querySelector('a').getAttribute('href');
+      if (link) {
         window.open(link, '_blank');
       }
-    });
-  }
+    }
+  });
 }
